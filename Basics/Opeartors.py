ca58dd89149876aedc77d49e3890d8335a81a55d
Lokesh Tiwari <tiwarilokesh796@gmail.com>
2026-02-01 23:00:45 +0530

    Create Opeartors.py

---
+++ Basics/Opeartors.py
@@ -0,0 +1,54 @@
+1. You are given two integer variables x and y. You need to perform the following operations:
+
+p = x + y : Addition
+q = x - y : Subtraction
+r = x * y :Multiplication
+s = x / y : Division
+t = x % y : Modulo
+
+x=int(input())
+y=int(input())
+#code here
+# Perform addition x+y below
+p = x+y
+# Perform subtraction x-y below
+q = x-y
+# Perform multiplication x*y below
+r = x*y
+# Perform integer divison x//y below
+s = x//y
+# Perform modulo operation x%y below
+t = x%y
+print(p, q, r, s, t)
+
+2.Logical operators and, or, not are used in condition checking. Like a and b checks if both a and b are True. First a is checked then b is checked. a or b checks if either of a or b is True. If one is True; it doesn't check for the other. not a complements the boolean value of a.
+Note: 0 and empty string are False and all other values are True.
+In this question you basically need to do
+a and b
+a or b
+not a
+
+a=int(input())
+b=int(input())
+#code here
+p = a and b
+#Do a or b below
+q = a or b 
+#Do not a below
+r = not a 
+#The code below prints the output. Don't change it!
+print(p,q,r)
+
+
+3. 
+
+
+
+
+
+
+
+
+
+
+
