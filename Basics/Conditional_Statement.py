6a94657d43a3b000e9e99e4a5e3027f04774fa54
Lokesh Tiwari <tiwarilokesh796@gmail.com>
2026-02-03 23:10:36 +0530

    Create Conditional_Statement.py

---
+++ Basics/Conditional_Statement.py
@@ -0,0 +1,9 @@
+1. There are two friends, John and Smith, and the parameters j_angry and s_angry to indicate if each is angry. You are in trouble if both of them are angry or no one of them is angry.
+
+Now, complete the function which returns true if you are in trouble, else return false
+
+def friends_in_trouble(j_angry, s_angry):
+    return j_angry == s_angry
+
+
+2.  
