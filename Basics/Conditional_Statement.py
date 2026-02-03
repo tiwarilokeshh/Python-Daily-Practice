4f1afdfdacd890d3dd27e2cf5aac742ac6d40dda
Lokesh Tiwari <tiwarilokesh796@gmail.com>
2026-02-03 23:26:37 +0530

    Update Conditional_Statement.py

--- Basics/Conditional_Statement.py
+++ Basics/Conditional_Statement.py
@@ -16,4 +16,33 @@ def checkOddEven(x):
     else:
         return "Odd"
 
-3. 
+
+3. ou have to take an interger input a, and then use the if statement to print "Big" (without quotes) 
+if the given number is greater than 100, and use the else statement to print "Small" (without quotes) when the number is smaller than or equal to 100.
+
+def check_number(a):
+    if a > 100:
+        return "Big"
+    else:
+        return "Small"
+        
+a = int(input())
+print(check_number(a))
+
+
+4. Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false if the number is odd.
+
+class Solution:
+    def isEven(self, n):
+        if n % 2 == 0:
+            return True
+        else:
+            return False
+
+
+5. 
+
+
+
+
+
