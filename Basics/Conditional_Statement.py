ab687999890d824948a6409a81904f9adc972184
Lokesh Tiwari <tiwarilokesh796@gmail.com>
2026-02-10 22:54:24 +0530

    Update Conditional_Statement.py

--- Basics/Conditional_Statement.py
+++ Basics/Conditional_Statement.py
@@ -77,7 +77,27 @@ Note: Do not add a new line at the end.
 
 
 
-7.
+7. Given an integer n, write a program to return the factorial of n. The Factorial of a number is the product of all the numbers from 1 to n.
+
+Note: 0 factorial is equal to 1.
+
+Example:
+
+Input: n = 5
+Output: 120
+Explanation: 1 * 2 * 3 * 4 * 5 = 120
+
+n = int(input())
+
+# Your code here
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+    
+print(fact)    
+
+
+8. 
     
 
 
